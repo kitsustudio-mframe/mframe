5ffee598523f40d42007598397bf3574da0b6a34
ZxyKira <danny1191365@kitsu-studio.net>
2023-06-10 22:19:13 +0800

    [add] first verison

---
+++ package-info.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2020 ZxyKira
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef MFRAME_E9F8A1F6_CAD3_404C_AE90_5DE9A53A7BCC
+#define MFRAME_E9F8A1F6_CAD3_404C_AE90_5DE9A53A7BCC
+
+/* ****************************************************************************************
+ * Include folder
+ */
+#include "./ctrl/package-info.h"
+#include "./hal/package-info.h"
+#include "./io/package-info.h"
+#include "./lang/package-info.h"
+#include "./util/package-info.h"
+
+/* ****************************************************************************************
+ * Include
+ */
+
+/* ****************************************************************************************
+ * End of file
+ */
+
+#endif /* MFRAME_E9F8A1F6_CAD3_404C_AE90_5DE9A53A7BCC */
